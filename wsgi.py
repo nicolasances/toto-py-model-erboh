843ee5bb5045f4bb578c3b9abd670f82ba6928b0
nicolasances <nicolas.matteazzi@gmail.com>
2020-02-17 19:59:07 +0100

    consumer starting in wsgi

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,13 @@ from toto_pubsub.consumer import TotoEventConsumer
 from predict.batch import predict as predict_batch
 from predict.single import predict as predict_single
 
+# Microservice name
+ms_name = 'model-erboh'
+
+print('Starting consumer')
+
+# Event Consumers
+TotoEventConsumer(ms_name, ['erbohBatchInferenceRequested', 'erbohPredictionRequested'], [predict_batch, predict_single])
+
 if __name__ == "__main__":
     app.run()
-
-    # Microservice name
-    ms_name = 'model-erboh'
-
-    # Event Consumers
-    TotoEventConsumer(ms_name, ['erbohBatchInferenceRequested', 'erbohPredictionRequested'], [predict_batch, predict_single])
