cefeaa56fb54ddc758c2b548ccdbf3cdc91d7092
nicolasances <nicolas.matteazzi@gmail.com>
2020-02-17 19:53:58 +0100

    added event mgt in wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,13 @@
-import app as whole_application
 from app import app
+from toto_pubsub.consumer import TotoEventConsumer
+from predict.batch import predict as predict_batch
+from predict.single import predict as predict_single
 
 if __name__ == "__main__":
     app.run()
+
+    # Microservice name
+    ms_name = 'model-erboh'
+
+    # Event Consumers
+    TotoEventConsumer(ms_name, ['erbohBatchInferenceRequested', 'erbohPredictionRequested'], [predict_batch, predict_single])
