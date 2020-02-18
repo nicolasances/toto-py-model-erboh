f979e4cffa11097fe7697679ded846230e31ef8c
nicolasances <nicolas.matteazzi@gmail.com>
2020-02-18 19:05:12 +0100

    logs

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from toto_pubsub.consumer import TotoEventConsumer
 from predict.batch import predict as predict_batch
 from predict.single import predict as predict_single
 
+print('app:first line)
+
 def create_app(): 
 
-    print('creating app')
-    
+    print('app:creating app')
+
     # Microservice name
     ms_name = 'model-erboh'
 
@@ -23,9 +25,15 @@ def create_app():
             "status": "running"
         })
 
+    print('app:returning app')
+
     return app
 
+print("app: calling create()")
 app = create_app()
+print("app: created")
 
 if __name__ == '__main__':
+    print("app: main")
     app.run(host='0.0.0.0', port=8080)
+    print("app: post run")
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from app import app
 
+print('wsgi:asd')
+
 if __name__ == "__main__":
+    print('wsgi:pre run')
     app.run()
+    print('wsdi:post run')
